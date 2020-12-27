f1899d1dc8d53294be7f9cfa762d7c82c1dffddf
Chris Dillon <chrispauldillon@live.com>
2020-12-27 22:25:06 +0000

    stephen does medicine

---
+++ pages/d7c952ee393049babf9464e07214ae69.tsx
@@ -0,0 +1,10 @@
+import { Heading, Text } from "@chakra-ui/react";
+import { CenterColumnFlex } from "../components/layout/Flexes";
+
+
+export default function d7c952ee393049babf9464e07214ae69() {
+      return (
+        <CenterColumnFlex>
+      <Heading>Stephen does medicine</Heading><Text>He really does</Text></CenterColumnFlex>)
+  }
+  
